060eb7f46efdebfb6ff74c911dad2867ecd8673d
kayomj <kayomj@gmail.com>
2018-07-17 01:34:55 -0300

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -3,7 +3,7 @@
 #loadkeys br-abnt2
 timedatectl set-ntp true
 
-mkfs.vfat -F /dev/sda5
+mkfs.fat -F32 /dev/sda5
 mkfs.ext4 -F /dev/sda6
 mount /dev/sda6 /mnt
 mkdir /mnt/boot
@@ -11,8 +11,7 @@ mount /dev/sda5 /mnt/boot
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkp
 echo "Server = http://archlinux.c3sl.ufpr.br/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
-pacstrap /mnt 
-base base-devel grub-efi-x86_64 efibootmgr intel-ucode os-prober git
+pacstrap /mnt base base-devel grub-efi-x86_64 efibootmgr intel-ucode os-prober git
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo "mount"
